5eead3524b5925d8869afa1002a42d8beb60ba3f
joaquingutierrez <joaquinguty@gmail.com>
2023-06-06 16:28:00 -0300

    Switch con animacion

--- src/components/ButtonSwitch/index.jsx
+++ src/components/ButtonSwitch/index.jsx
@@ -4,12 +4,23 @@ import "./style.css"
 
 const ButtonSwitch = ({ handleButton }) => {
 
-
+    const animationSwitch = () => {
+        handleButton()
+        /*         const checkboxSwitch = document.getElementById("checkboxSwitch")
+                checkboxSwitch.classList.add("animationForSwitchOn") */
+    }
 
     return (
-        <label className="switch" onChange={handleButton}>
-            <input type="checkbox" className="checkbox" />
-            <span className="textSlider"></span>
+        <label className="switch" onChange={animationSwitch}>
+            <div className="checkboxSwitch">
+                <input type="checkbox" className="checkbox" />
+                <div className="switchONContainer">
+                    <span className="textSlider switchON">ON</span>
+                </div>
+                <div className="switchOFFContainer">
+                    <span className="textSlider switchOFF">OFF</span>
+                </div>
+            </div>
         </label>
     )
 }
